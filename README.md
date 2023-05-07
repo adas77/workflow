05835b85398e25c239742bdb9e34f1d8e9eb85f3
Adam Pilewski <apilewski01@gmail.com>
2023-05-07 17:37:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Workflow
+## Template
+- [Create T3 App](https://create.t3.gg/)
+```bash
+npm create t3-app@latest
+```
+
+## Google Integration
+- [Google Calendar API](https://www.youtube.com/watch?v=c2b2yUNWFzI)
+- [Google Next Auth](https://www.telerik.com/blogs/how-to-implement-google-authentication-nextjs-app-using-nextauth)
+## ENV Variables
+```bash
+cat .env.example > .env
+```
---
+++ src/server/auth.ts
@@ -0,0 +1,48 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type DefaultSession,
+  type NextAuthOptions,
+} from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import { env } from "~/env.mjs";
+import { prisma } from "~/server/db";
+
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    DiscordProvider({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+
+  ],
+};
+
+
+export const getServerAuthSession = (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return getServerSession(ctx.req, ctx.res, authOptions);
+};
